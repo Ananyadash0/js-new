f1065abc01e4e83c7a027f9c1e53a2619dbf9118
Ananya Dash <147904973+Ananyadash0@users.noreply.github.com>
2026-01-21 05:31:54 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId ="01"
+let accountEmail = "ananya@google.com"
+var accountPassword = "ananya"
+
+accountCity ="Bhubaneswar"
+
+// accountId = "02" // Not allowed
+
+console.log(accountId)
+
+console.log([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3 ) // we are using node.js, not browsers
+
+console.log (3+3)
+console.log("Ananya")
+
+
+let name = "Ananya"
+let age =22
+let isLoggedIn = true
+let state;
+
+// number = 2 to the power 53 (range)
+// bigint =
+// String = "" or '' ("" is more preferable)
+// boolean = true / false
+// null = standalone value
+// undefined 
+// symbol = unique
+
+
+// object 
+
+console.log (typeof "Hello")
+console.log (typeof age)
+console.log (typeof null)
+console.log (typeof undefined)
