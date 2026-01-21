e1f21eeabc2afd81d8e1d6504b402c5cd4095ee0
Ananya Dash <147904973+Ananyadash0@users.noreply.github.com>
2026-01-21 06:09:47 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,80 @@
+let score = 33
+let score1 = "33"
+let score2 = "33xyz"
+let score3 = null
+let score4 = undefined
+let score5 = true
+
+
+console.log(typeof score)
+console.log(typeof (score))
+let value = Number(score)
+console.log(typeof value)
+console.log(value)
+
+console.log(typeof score1)
+console.log(typeof (score1))
+let value1 = Number(score1)
+console.log(typeof value1)
+console.log(value1)
+
+console.log(typeof score2)
+console.log(typeof (score2))
+let value2 = Number(score2)
+console.log(typeof value2)
+console.log(value2)
+
+console.log(typeof score3)
+console.log(typeof (score3))
+let value3 = Number(score3)
+console.log(typeof value3)
+console.log(value3)
+
+console.log(typeof score4)
+console.log(typeof (score4))
+let value4 = Number(score4)
+console.log(typeof value4)
+console.log(value4)
+
+console.log(typeof score5)
+console.log(typeof (score5))
+let value5 = Number(score5)
+console.log(typeof value5)
+console.log(value5)
+
+// "33" = number
+// "33abc" = Nan (Not a number)
+// null = 0
+// undefined = NaN
+// true = 1, false = 0
+
+let isLoggedIn = 1
+let isLoggedIn1 = ""
+let isLoggedIn2 = "Ananya"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1)
+
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2)
+
+
+// true = 1, false = 0
+// "" = false
+// "Ananya" = true
+
+let anyNumber = 95
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+let anything = true
+let changeNumber = String(anything)
+console.log(anything)
+console.log(changeNumber)
+console.log(typeof changeNumber)
