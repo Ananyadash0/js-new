cb26bd99ac81fc6159f19bae7d03528bf9711e73
Ananya Dash <147904973+Ananyadash0@users.noreply.github.com>
2026-02-10 07:19:30 +0000

    Dates in Javascript

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,20 @@
+// Date
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString);
+console.log(myDate.toLocaleDateString);
+console.log(myDate.toJSON);
+console.log(myDate.toISOString);
+console.log(typeof myDate)
+
+let myCreatedDate = new Date (2023, 0, 23)
+console.log(myCreatedDate);
+console.log(myCreatedDate.toDateString());
+
+let myCreatedDatee = new Date (01-20-2024)
+console.log(myCreatedDatee);
+console.log(myCreatedDatee.toDateString());
+console.log(myCreatedDatee.toLocaleString());
