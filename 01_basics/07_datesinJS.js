28a47854e44bfe3888c6adab327989f080e6eabd
Ananya Dash <147904973+Ananyadash0@users.noreply.github.com>
2026-02-20 05:18:01 +0000

    Changes made in dates

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -1,20 +1,34 @@
 // Date
 
 let myDate = new Date()
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString);
-console.log(myDate.toLocaleDateString);
-console.log(myDate.toJSON);
-console.log(myDate.toISOString);
-console.log(typeof myDate)
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString);
+// console.log(myDate.toLocaleDateString);
+// console.log(myDate.toJSON);
+// console.log(myDate.toISOString);
+// console.log(typeof myDate)
 
-let myCreatedDate = new Date (2023, 0, 23)
-console.log(myCreatedDate);
-console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date (2023, 0, 23)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
 
-let myCreatedDatee = new Date (01-20-2024)
-console.log(myCreatedDatee);
-console.log(myCreatedDatee.toDateString());
-console.log(myCreatedDatee.toLocaleString());
+let myCreatedDatee = new Date ("01-20-2024")
+// console.log(myCreatedDatee);
+// console.log(myCreatedDatee.toDateString());
+// console.log(myCreatedDatee.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDatee.getTime());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+})
